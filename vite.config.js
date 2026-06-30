ca03c2238695db1df381ab5ddcbdc43340068ff9
chivalry1314 <506249164@qq.com>
2026-06-30 16:25:31 +0800

    config: set GitHub Pages base path to /biopsychology/

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/biopsychology/',
   build: {
     chunkSizeWarningLimit: 800
-  },
-  // 如果部署到 GitHub Pages 项目页面（如 https://用户名.github.io/仓库名/）
-  // 请取消下面注释并替换为仓库名称：
-  // base: '/psychology/',
+  }
 })
